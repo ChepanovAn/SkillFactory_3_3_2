9ca0f30090dace40d11c15281c04604b2794267c
Анатолий <Chepanov199287@gmail.com>
2023-03-20 22:31:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_3_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jane";
+            int age = 27;
+            bool answer = true;
+            double size = 37.5;
+
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My age is " + age);
+            Console.WriteLine("Do you have a pet? " + answer);
+            Console.WriteLine("My shoe size is " + size);
+            Console.ReadLine();
+        }
+    }
+}
